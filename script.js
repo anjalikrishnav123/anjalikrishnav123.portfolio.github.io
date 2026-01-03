7c2c46466bc2265af15156ab789296db14b6a09b
Arathi Krishna V <70818388+arathikrishnav@users.noreply.github.com>
2026-01-03 20:49:41 +0530

    added portfolio content, files

--- script.js
+++ script.js
@@ -50,3 +50,14 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+//Open external links in a ne tab
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('a[href]').forEach(link => {
+      // Only external links
+      if (link.hostname && link.hostname !== window.location.hostname) {
+        link.target = "_blank";
+        link.rel = "noopener noreferrer";
+      }
+    });
+  });
